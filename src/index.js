c0c48bde287360d9c1258a6cf08d6ce73de56bfa
Aniket Singla <imaniketsingla8@gmail.com>
2024-10-13 10:54:28 +0530

    connection to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//require('dotenv').config({path: './env'}); (because it is exploiting the consistency of the code)
+import dotenv from "dotenv" //to do this we have to do 2 more steps 1) config dotenv below and 2) add experimental feature in package.json file
+import connectDB from "./db/index.js";
+
+dotenv.config({path: './env'})
+//The below code was for practice and the better approach was used after this as this code was polluting the index file as we used a lot of things in one file
+/*
+import express from "express";
+const app = express();  
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR:", error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error 
+    }
+})()
+*/
+
+connectDB();
