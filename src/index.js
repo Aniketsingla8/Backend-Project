f8b2ffd6b65ff6078355d2fd52a22fec9932f0d3
Aniket Singla <imaniketsingla8@gmail.com>
2024-10-17 11:43:26 +0530

    utils files made added generalised api handlers

--- src/index.js
+++ src/index.js
@@ -26,4 +26,12 @@ const app = express();
 })()
 */
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is listening on ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection error: ", error)
+})
